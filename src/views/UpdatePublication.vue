ee5267213cf3b82df99fde6dd041eb3a54ccf0c5
KraenauDiegoDoApps <diego.kraeneau@doapps.me>
2021-06-19 12:59:56 -0500

    Update section and myPublications section

---
+++ src/views/UpdatePublication.vue
@@ -0,0 +1,175 @@
+<template>
+  <v-app id="inspire">
+    <v-main>
+      <v-container fluid>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card-text>
+              <v-form>
+                <h2>Add publication</h2>
+                <br />
+                <v-text-field
+                  name="Title"
+                  label="Title"
+                  type="text"
+                  v-model="publication.title"
+                ></v-text-field>
+                <v-textarea
+                  name="input-7-1"
+                  label="First Paragraph"
+                  rows="4"
+                  no-resize
+                  v-model="publication.firstParagraph"
+                ></v-textarea>
+                <v-text-field
+                  name="url"
+                  label="Image URL"
+                  type="url"
+                  v-model="publication.imageUrl"
+                ></v-text-field>
+                <v-row>
+                  <v-col class="text-right">
+                    <v-btn @click="addSection()">+ Add section</v-btn>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col
+                    class="text-center"
+                    v-if="publication.sectionPublications.length === 0"
+                  >
+                    <h2>You can add sections</h2>
+                  </v-col>
+                </v-row>
+                <template
+                  v-for="(item, index) in publication.sectionPublications"
+                >
+                  <v-row :key="index" class="text-left">
+                    <v-col>
+                      <div
+                        :style="{
+                          display: 'flex',
+                          alignItems: 'center',
+                          justifyContent: 'space-between',
+                        }"
+                      >
+                        <h4>Section {{ index + 1 }}</h4>
+                        <v-btn icon color="red" @click="deleteSection(index)">
+                          <v-icon>mdi-trash-can</v-icon>
+                        </v-btn>
+                      </div>
+                      <v-text-field
+                        name="Subtitle"
+                        label="Subtitle"
+                        type="text"
+                        v-model="item.subtitle"
+                      ></v-text-field>
+                      <v-textarea
+                        name="input-7-1"
+                        label="Paragraph"
+                        rows="3"
+                        no-resize
+                        v-model="item.paragraph"
+                      ></v-textarea>
+                      <v-text-field
+                        name="url"
+                        label="Image URL"
+                        type="url"
+                        v-model="item.imageUrl"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </template>
+                <v-card-actions>
+                  <v-btn
+                    class=""
+                    color="teal lighten-2"
+                    @click="updatePublication()"
+                    :style="{ margin: '20px auto' }"
+                  >
+                    <v-progress-circular
+                      v-if="loading"
+                      indeterminate
+                      color="primary"
+                    ></v-progress-circular>
+                    Add
+                  </v-btn>
+                </v-card-actions>
+              </v-form>
+            </v-card-text>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import axios from "axios";
+import { BASE_URL } from "../variables/variables";
+import Swal from "sweetalert2";
+
+export default {
+  name: "UpdatePublication",
+  data: () => ({
+    loading: false,
+    Section: {
+      subtitle: "",
+      paragraph: "",
+      imageUrl: "",
+    },
+    publication: {
+      id: 0,
+      title: "",
+      firstParagraph: "",
+      imageUrl: "",
+      sectionPublications: [],
+      idUser: 0,
+    },
+  }),
+  methods: {
+    getPublication() {
+      this.loadingPublication = true;
+      const id = this.$route.params.id;
+      axios.get(`${BASE_URL}/publications/find/${id}`).then((response) => {
+        this.publication = response.data;
+        this.loadingPublication = false;
+      });
+    },
+    addSection() {
+      let s = {
+        subtitle: "",
+        paragraph: "",
+        imageUrl: "",
+      };
+
+      this.publication.sectionPublications.push(s);
+    },
+    updatePublication() {
+      this.loading = true;
+      axios
+        .put(
+          `${BASE_URL}/publications/update/${this.publication.id}`,
+          this.publication
+        )
+        .then((response) => {
+          if (response.data) {
+            Swal.fire("Good job!", "Yo updated a publication", "success");
+          }
+        })
+        .catch(() => {
+          this.loadingLogin = false;
+        });
+      this.loading = false;
+    },
+    deleteSection(index) {
+      this.publication.sectionPublications.splice(index, 1);
+    },
+  },
+  mounted() {
+    this.getPublication();
+  },
+  computed: {},
+};
+</script>
+
+<style scoped></style>
