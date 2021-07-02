c39a934cc6f435d8cc99b72a489cff2fa2de1128
ticlawin <ticlawin@gmail.com>
2021-07-01 21:33:42 -0500

    Update variables.js
    se actualizó URL API

--- src/variables/variables.js
+++ src/variables/variables.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://knowledge-cybersecurity-rest.azurewebsites.net'
+export const BASE_URL = 'https://maao-knowledge-api.herokuapp.com'
 
 //export const BASE_URL = 'http://26.38.36.67:4899'
